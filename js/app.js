4b0249a2fde019e3c01479c991c7f302e9e527fc
Ben <ben-smith@blueyonder.co.uk>
2015-06-10 17:00:55 +0100

    Adding reviews to the display, and using form and validation

--- js/app.js
+++ js/app.js
@@ -20,20 +20,48 @@
         };
     });
 
+    app.controller('ReviewController', function() {
+        this.review = {};
+
+        this.addReview = function(product) {
+            product.reviews.push(this.review);
+            this.review = {};
+        };
+    });
+
     var gems = [
         {
             name: 'Dodecahedron',
             price: 2,
             description: 'This is the dodecahedron gem',
             canPurchase: true,
-            soldOut: false
+            soldOut: false,
+            reviews: [
+                {
+                    stars: 5,
+                    body: 'I love this product!',
+                    author: 'ben@ben.com'
+                },
+                {
+                    stars: 4,
+                    body: 'It\'s pretty good!',
+                    author: 'nicola@ben.com'
+                }
+            ]
         },
         {
             name: 'Pentagonal Gem',
             price: 5.95,
             description: 'Here is the Pentagonal Gem',
             canPurchase: false,
-            soldOut: false
+            soldOut: false,
+            reviews: [
+                {
+                    stars: 1,
+                    body: 'This gen sucks',
+                    author: 'Kimberly@ben.com'
+                }
+            ]
         }
     ];
 })();
